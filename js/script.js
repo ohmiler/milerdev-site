d1bc13a47f40c66085bfc1723087cecb9e2c4c3f
Patiphan Phengpao <misterpatipan123@gmail.com>
2020-04-27 12:42:59 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay: true,
+    margin:10,
+    nav: true,
+    dots: true,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
